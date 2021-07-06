60be488fc4c6ce228c02d565633c0bf919971b71
Prabhjeet Singh <prabhjeet2911@gmail.com>
2021-07-06 17:19:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 st.set_option('deprecation.showfileUploaderEncoding', False)
 # Load the pickled model
-model = pickle.load(open('/content/kmeansclusterassignment.pkl','rb'))   
-dataset= pd.read_csv('/content/clustering Dataset3.csv')
+model = pickle.load(open('kmeansclusterassignment.pkl','rb'))   
+dataset= pd.read_csv('clustering Dataset3.csv')
 X = dataset.iloc[:,1:].values
 # Taking care of missing data
 #handling missing data (Replacing missing data with the mean value)  
